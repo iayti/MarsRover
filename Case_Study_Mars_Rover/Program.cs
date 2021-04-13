c7f193d80dcc622d3798dd82cde94fca5b6c228b
iayti <ilkerayti@gmail.com>
2021-04-13 23:05:48 +0300

    Case Study completed. Refactoring and Testing will be added.

--- Case_Study_Mars_Rover/Program.cs
+++ Case_Study_Mars_Rover/Program.cs
@@ -12,102 +12,146 @@ namespace Case_Study_Mars_Rover
 
             while (true)
             {
+                Console.WriteLine("Enter the dimensions: ");
                 //Get dimension of the plateau on Mars
                 string dimension = Console.ReadLine()?.Trim();
 
                 if (!DimensionFormatCheck(dimension))
-                    Console.WriteLine("Please write the dimension of the plateau on Mars. For example: 5 5");
-                else
                 {
-                    int plateauAreaX = 0;
-                    int plateauAreaY = 0;
+                    Console.WriteLine("Please write the dimension of the plateau on Mars. For example: 5 5");
+                    continue;
+                }
 
-                    if (dimension != null)
-                    {
-                        string[] dimensionArray = dimension.Split(" ");
+                int plateauAreaX = 0;
+                int plateauAreaY = 0;
 
-                        plateauAreaX = int.Parse(dimensionArray[0]);
-                        plateauAreaY = int.Parse(dimensionArray[1]);
-                    }
+                if (dimension != null)
+                {
+                    string[] dimensionArray = dimension.Split(" ");
+
+                    plateauAreaX = int.Parse(dimensionArray[0]);
+                    plateauAreaY = int.Parse(dimensionArray[1]);
+                }
 
 
-                    var roverList = new List<Rover>();
+                var roverList = new List<Rover>();
 
-                    #region While Loop
+                while (true)
+                {
+                    Console.WriteLine("Enter the rover position and direction: ");
 
                     string positionDirection = Console.ReadLine()?.Trim();
 
                     if (!PositionDirectionFormatCheck(positionDirection))
-                        Console.WriteLine("Please write the rover position and direction correctly. For example: 5 5 N, N: North, E: East, S: South, W: West");
-                    else
                     {
-                        if (positionDirection != null)
-                        {
-                            string[] positionDirectionArray = positionDirection.Split(" ");
-
-                            int roverLocationX = int.Parse(positionDirectionArray[0]);
-                            int roverLocationY = int.Parse(positionDirectionArray[1]);
-                            string roverDirection = positionDirectionArray[2];
-
-                            Rover rover = new Rover(roverLocationX, roverLocationY, roverDirection);
-
-                            string roverRoute = Console.ReadLine()?.Trim();
-
-                            if (roverLocationX > plateauAreaX || roverLocationX < 0 || roverLocationY > plateauAreaY || roverLocationY < 0)
-                            {
-                                //Try again break
-                                Console.WriteLine("The rover is not in the plateau");
-                            }
-
-                            if (roverRoute != null)
-                            {
-                                foreach (var route in roverRoute)
-                                {
-                                    if (route == 'L' || route == 'R')
-                                    {
-                                        rover.Direction = CalculateDirection(rover.Direction, route);
-                                    }
-                                    else if (route == 'M')
-                                    {
-                                        MoveRover(rover);
-                                    }
-                                }
-                                //rover can be created and added in the end of the line.
-                                //
-                                roverList.Add(rover);
-                            }
-                        }
+                        Console.WriteLine("Please write the rover position and direction correctly. For example: 5 5 N, N: North, E: East, S: South, W: West");
+                        continue;
                     }
 
-                    #endregion
-                    
-                    //TODO: after the loop ends Console.WriteLine(Each.roverlist.LocationDirection)
+                    if (positionDirection != null)
+                    {
+                        string[] positionDirectionArray = positionDirection.Split(" ");
+
+                        int roverLocationX = int.Parse(positionDirectionArray[0]);
+                        int roverLocationY = int.Parse(positionDirectionArray[1]);
+                        string roverDirection = positionDirectionArray[2];
+
+                        //Rover rover = new Rover(roverLocationX, roverLocationY, roverDirection);
+
+                        string roverRoute = Console.ReadLine()?.Trim();
+
+                        if (roverLocationX > plateauAreaX || roverLocationX < 0 || roverLocationY > plateauAreaY || roverLocationY < 0)
+                        {
+                            Console.WriteLine("The rover is not in the plateau");
+                            continue;
+                        }
+
+                        if (roverRoute == null)
+                            continue;
+
+                        foreach (var route in roverRoute)
+                        {
+                            switch (route)
+                            {
+                                case 'L' or 'R':
+                                    roverDirection = CalculateDirection(roverDirection, route);
+                                    break;
+                                case 'M':
+                                    var result = MoveRover(roverLocationX, roverLocationY, roverDirection);
+                                    roverLocationX = result.Item1;
+                                    roverLocationY = result.Item2;
+                                    break;
+                            }
+                        }
+
+                        Rover rover = new Rover(roverLocationX, roverLocationY, roverDirection);
+                        roverList.Add(rover);
+
+                        Add_Rover:
+                        Console.WriteLine("Rover added to the plateau on Mars!");
+                        Console.WriteLine("If you want to add one more rover, please write 'add'");
+                        Console.WriteLine("If there is enough rover, please write 'go'");
+
+                        string state = Console.ReadLine()?.Trim();
+
+                        if (!string.IsNullOrEmpty(state))
+                        {
+                            if (state == "add")
+                                continue;
+                            if (state == "go")
+                                break;
+
+                            goto Add_Rover;
+                        }
+                    }
                 }
+
+                foreach (var rover in roverList)
+                {
+                    Console.WriteLine(rover.LocationDirection);
+                }
+
+                Console.WriteLine("If you want to exit the program, please write 'exit'");
+                Console.WriteLine("If you want to continue the program, please write any key.");
+
+                string programState = Console.ReadLine()?.Trim();
+
+                if (string.IsNullOrEmpty(programState))
+                    continue;
+                if (programState == "exit")
+                    break;
             }
 
+            Console.WriteLine("Program closed!");
+            Console.ReadLine();
         }
 
         /// <summary>
-        /// Drives the rover in the given direction
+        /// Drives the rover in the given direction.
         /// </summary>
-        /// <param name="rover"></param>
-        public static void MoveRover(Rover rover)
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+        public static (int, int) MoveRover(int x, int y, string direction)
         {
-            switch (rover.Direction)
+            switch (direction)
             {
                 case "N":
-                    rover.Y += 1;
+                    y += 1;
                     break;
                 case "E":
-                    rover.X += 1;
+                    x += 1;
                     break;
                 case "S":
-                    rover.Y -= 1;
+                    y -= 1;
                     break;
                 case "W":
-                    rover.X -= 1;
+                    x -= 1;
                     break;
             }
+
+            return (x, y);
         }
 
         /// <summary>
@@ -125,7 +169,6 @@ namespace Case_Study_Mars_Rover
 
             for (int i = 0; i < 4; i++)
             {
-
                 if (roverDirection == allDirections[i].ToString())
                 {
                     responseDirection = route switch
